4ebaa64d78b6bcf15663b195c5e366c9ffdb98f1
gclausen0272 <54636091+gclausen0272@users.noreply.github.com>
2022-03-17 20:01:37 -0500

    Add files via upload

---
+++ Beverage.java
@@ -0,0 +1,14 @@
+
+public class Beverage {
+    private int cost;
+    public Beverage(int cost){
+        this.cost = cost;
+    }
+    
+    public boolean buyBeverage(int money){
+        if(money>=cost){
+            return true;
+        }
+        return false;
+    }
+}
